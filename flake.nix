240e0d0c8d81797fa3b361e5ce4a4155951accf1
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2025-03-18 14:46:26 +0100

    Point to the correct repo

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "systemd-sysupdate / systemd-repart Example";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:blitz/nixpkgs?ref=oceansprint-evil-toplevel";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
